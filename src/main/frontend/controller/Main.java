61b9457f2b1284e7c228908354eb2f18150cad34
HONG-XIN <honsinwang@gmail.com>
2019-11-20 00:05:17 -0500

    javafx test 2

--- src/main/frontend/controller/Main.java
+++ src/main/frontend/controller/Main.java
@@ -6,40 +6,29 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.util.ArrayList;
+
 public class Main extends Application {
 
-    private static Stage stage;
-
-    private static Scene mainScene;
-    private static Scene detailsScene;
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        stage = primaryStage;
 
-        stage.setTitle("ExampleLOO");
+
+        primaryStage.setTitle("ExampleLOO");
 
         Parent fxmlMain = FXMLLoader.load(getClass().getResource("../view/main_screen.fxml"));
-        mainScene = new Scene(fxmlMain, 640, 400);
 
-        Parent fxmlDetails = FXMLLoader.load(getClass().getResource("../view/details_screen.fxml"));
-        detailsScene = new Scene(fxmlDetails, 640, 400);
 
-        stage.setScene(mainScene);
-        stage.show();
+        primaryStage.setScene(new Scene(fxmlMain, 640, 400));
+        primaryStage.show();
     }
 
-    public static void changeScreen(String scr){
-        switch (scr){
-            case "main":
-                stage.setScene(mainScene);
-                break;
-            case "details":
-                stage.setScene(detailsScene);
-        }
-    }
 
     public static void main(String[] args) {
         launch(args);
     }
+
+
+
 }
