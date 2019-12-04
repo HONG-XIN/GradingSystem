7518f70902af1c1fcb000eef424636072af4ed76
Yicheng Li <yichengli1997@hotmail.com>
2019-12-03 19:35:50 -0500

    update assignment score

--- src/main/backend/CourseGrade.java
+++ src/main/backend/CourseGrade.java
@@ -16,4 +16,87 @@ public class CourseGrade {
         this.bonus = 0;
         this.comment = "";
     }
+    CourseGrade(int courseID, int studentID, int finalscore){
+        this.courseID = courseID;
+        this.studentID = studentID;
+        this.finalscore = finalscore;
+        if(finalscore > 75){
+            this.letter = "A";
+        }
+        else if(finalscore > 50){
+            this.letter = "B";
+        }
+        else if(finalscore > 25){
+            this.letter = "C";
+        }
+        else {
+            this.letter = "D";
+        }
+        this.bonus = 0;
+        this.comment = "";
+    }
+    CourseGrade(int courseID, int studentID, String letter){
+        this.courseID = courseID;
+        this.studentID = studentID;
+        this.letter = letter;
+        if(letter == "A"){
+            this.finalscore = 100;
+        }
+        else if(letter == "B"){
+            this.finalscore = 75;
+        }
+        else if(letter == "C"){
+            this.finalscore = 50;
+        }
+        else {
+            this.finalscore = 25;
+        }
+        this.bonus = 0;
+        this.comment = "";
+    }
+    CourseGrade(int courseID, int studentID, int finalscore, String letter){
+        this.courseID = courseID;
+        this.studentID = studentID;
+        this.finalscore = finalscore;
+        this.letter = letter;
+        this.bonus = 0;
+        this.comment = "";
+    }
+    CourseGrade(int courseID, int studentID, int finalscore, String letter, int bonus){
+        this.courseID = courseID;
+        this.studentID = studentID;
+        this.finalscore = finalscore;
+        this.letter = letter;
+        this.bonus = bonus;
+        this.comment = "";
+    }
+    CourseGrade(int courseID, int studentID, int finalscore, String letter, int bonus, String comment){
+        this.courseID = courseID;
+        this.studentID = studentID;
+        this.finalscore = finalscore;
+        this.letter = letter;
+        this.bonus = bonus;
+        this.comment = comment;
+    }
+    //mutator
+
+    //accesser
+    public int getCourseID(){
+        return this.courseID;
+    }
+    public int getStudentID(){
+        return this.studentID;
+    }
+    public int getFinalscore(){
+        return this.finalscore;
+    }
+    public String getLetter(){
+        return this.letter;
+    }
+    public int getBonus(){
+        return this.bonus;
+    }
+    public String getComment(){
+        return this.comment;
+    }
 }
