37319dd6f3bf42e1a06ad06ac75cfb148f0b5994
Yicheng Li <yichengli1997@hotmail.com>
2019-12-03 21:34:02 -0500

    id class

---
+++ src/main/backend/ID.java
@@ -0,0 +1,21 @@
+package main.backend;
+
+public class ID {
+    private int number;
+    ID(){
+       number = 0;
+    }
+    ID(int number){
+        this.number = number;
+    }
+    public boolean checkSame(ID id){
+        if(this.number == id.number){
+            return true;
+        }
+        return false;
+    }
+    //accesser
+    public String getID(){
+        return String.format("06",this.number);
+    }
+}
