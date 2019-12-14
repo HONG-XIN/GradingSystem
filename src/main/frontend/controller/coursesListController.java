29f76e2281637fd0bca71083fe6e48d87cf57f7e
HONG-XIN <honsinwang@gmail.com>
2019-12-14 15:42:44 -0500

    start courses list

--- src/main/frontend/controller/coursesListController.java
+++ src/main/frontend/controller/coursesListController.java
@@ -1,9 +1,29 @@
 package main.frontend.controller;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
+
+import java.util.ArrayList;
 
 public class coursesListController {
+
+    @FXML
+    ChoiceBox cbSemester;
+
+    @FXML
+    protected void initialize() {
+        cbSemester.setItems(FXCollections.observableArrayList("1", "2"));
+        cbSemester.getSelectionModel().selectedIndexProperty().addListener(new ChangeListener<Number>() {
+            @Override
+            public void changed(ObservableValue<? extends Number> observableValue, Number number, Number t1) {
+                System.out.println(t1.intValue());
+            }
+        });
+    }
 
     @FXML
     protected void btNewCourse(ActionEvent e) {
