68c5412b124e2f3e4644045632d03e11ccd1fadb
zhuziyu0727 <55662983+zhuziyu0727@users.noreply.github.com>
2019-12-15 15:16:55 -0500

    update gitignore;

--- src/main/backend/Score.java
+++ src/main/backend/Score.java
@@ -40,31 +40,4 @@ public class Score {
         }
     }
 
-    //DB function
-    //from RAM to DB
-    public Document write(){
-        Document ScoreDoc = new Document();
-        ScoreDoc.put("value", getValue());
-        if (this.type.equals(ScoreType.PERCENTAGE)){
-            ScoreDoc.put("type", "PERCENTAGE");
-        }
-        else if (this.type.equals(ScoreType.DEDUCTION)){
-            ScoreDoc.put("type", "DEDUCTION");
-        }
-        return ScoreDoc;
-    }
-
-    //from DB to RAM
-    public void read(Document doc){
-        if(doc != null){
-            setValue((double) doc.get("value"));
-            String t = (String)doc.get("type");
-            if (t.equals("PERCENTAGE")){
-                this.type = ScoreType.PERCENTAGE;
-            }
-            else if (t.equals("DEDUCTION")){
-                this.type = ScoreType.DEDUCTION;
-            }
-        }
-    }
 }
